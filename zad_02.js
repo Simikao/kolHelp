1d58c4684dd0eb9fa06fb543f038d7abf3451df1
John Doe <s.wierzejski.420@studms.edu.ug.pl>
2023-02-21 11:20:37 +0100

    First few exercises and Init

---
+++ zad_02.js
@@ -0,0 +1,12 @@
+let testVar = 5;
+if (testVar > 0){
+    console.log("Liczba jest dodatnia");
+}else{
+    console.log("Liczba jest niedodatnia");
+};
+
+if (testVar % 2 == 0){
+    console.log("Liczba jest parzysta");
+}else{
+    console.log("Liczba jest nieparzysta");
+}
